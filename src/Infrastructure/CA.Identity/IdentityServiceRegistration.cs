1ac412b9335290586898cc9805006e780ff46def
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-09-25 12:17:22 +0330

    add service for identity

---
+++ src/Infrastructure/CA.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,60 @@
+﻿using CA.Application.Contracts.Identity;
+using CA.Identity.DbContexts;
+using CA.Identity.Models;
+using CA.Identity.Services;
+using CA.Identity.Services.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CA.Identity
+{
+
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("IdentityConnectionString")));
+
+
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
+
+            //services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IRoleClaimService, RoleClaimService>();
+            services.AddTransient<ITokenService, IdentityService>();
+            services.AddTransient<IRoleService, RoleService>();
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IUserService, UserService>();
+
+            //services.AddTransient<IAuditService, AuditService>();
+            return services;
+        }
+
+    }
+
+    public class IdentityDbContextFactor : IDesignTimeDbContextFactory<IdentityDbContext>
+    {
+        public IdentityDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<IdentityDbContext>();
+            var connectionString = configuration.GetConnectionString("IdentityConnectionString");
+            builder.UseSqlServer(connectionString);
+            //builder.UseSqlServer("Data Source=.;Initial Catalog=Fuzzy;User ID=sa;Password=1;Persist Security Info=True");
+            return new IdentityDbContext(builder.Options);
+
+        }
+    }
+}
