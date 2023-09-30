388c18e0fde72455ad4bea58d1b125eb06137288
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-09-30 09:51:10 +0330

    add test for mapping

---
+++ test/UnitTest/Mapping/MappingTest.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CA.Application.DTOs.Ent;
+using CA.Application.Profiles;
+using CA.Domain.Ent;
+using System.Runtime.Serialization;
+
+namespace UnitTest.Mapping
+{
+    public class MappingTests
+    {
+
+        private static IConfigurationProvider _configuration;
+        private static IConfigurationProvider configuration
+        {
+            get
+            {
+                if (_configuration == null)
+                {
+                    _configuration = new MapperConfiguration(cfg =>
+                    {
+                        cfg.AddProfile<MappingProfile>();
+                    });
+                }
+                return _configuration;
+            }
+        }
+        private static IMapper _mapper;
+        private static IMapper mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                {
+                    _mapper = configuration.CreateMapper();
+                }
+                return _mapper;
+            }
+
+        }
+        [Theory]
+        [InlineData(typeof(SelectionDto), typeof(Selection))]
+        [InlineData(typeof(Selection), typeof(SelectionDto))]
+        [InlineData(typeof(SelectionCreateDto), typeof(Selection))]
+        [InlineData(typeof(SelectionUpdateDto), typeof(Selection))]
+        public void Map_SourceToDestination_ExistConfiguration(Type origin, Type destination)
+        {
+            var instance = FormatterServices.GetUninitializedObject(origin);
+
+            mapper.Map(instance, origin, destination);
+        }
+    }
+}
