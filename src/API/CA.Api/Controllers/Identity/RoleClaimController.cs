6d31188aa598af316517022097c10e9653a1ec1a
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-10-04 14:36:21 +0330

    edit roleClaimController add ProducesResponseType

--- src/API/CA.Api/Controllers/Identity/RoleClaimController.cs
+++ src/API/CA.Api/Controllers/Identity/RoleClaimController.cs
@@ -1,6 +1,8 @@
 ﻿using CA.Application.DTOs.Identity.Requests;
+using CA.Application.DTOs.Identity.Responses;
 using CA.Application.Features.Identity.RoleClaim.Commands;
 using CA.Application.Features.Identity.RoleClaim.Queries;
+using CA.Domain.Base;
 using CA.Domain.Constants.Permission;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -22,64 +24,54 @@ namespace CA.Api.Controllers.Identity
             _httpContextAccessor = httpContextAccessor;
         }
 
-        /// <summary>
-        /// Get All Role Claims(e.g. Alternative Create Permission)
-        /// </summary>
-        /// <returns>Status 200 OK</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<RoleClaimResponse>), StatusCodes.Status200OK)]
         [Authorize(Policy = Permissions.RoleClaimsPermissions.View)]
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<ActionResult<List<RoleClaimResponse>>> GetAll()
         {
             var roleClaims = await _mediator.Send(new GetAllRoleClaimsQuery());
             return Ok(roleClaims);
         }
 
-        /// <summary>
-        /// Get All Role Claims By Id
-        /// </summary>
-        /// <param name="roleId"></param>
-        /// <returns>Status 200 OK</returns>
+
+        [ProducesResponseType(typeof(List<RoleClaimResponse>), StatusCodes.Status200OK)]
         [Authorize(Policy = Permissions.RoleClaimsPermissions.View)]
         [HttpGet("{roleId}")]
-        public async Task<IActionResult> GetAllByRoleId(string roleId)
+        public async Task<ActionResult<List<RoleClaimResponse>>> GetAllByRoleId(string roleId)
         {
             var response = await _mediator.Send(new GetAllRoleClaimsByRoleIdQuery() { roleId = roleId });
             return Ok(response);
         }
 
+        [ProducesResponseType(typeof(List<Claims>), StatusCodes.Status200OK)]
         [Authorize(Policy = Permissions.RoleClaimsPermissions.View)]
         [HttpGet("GetAllClaims")]
-        public async Task<IActionResult> GetAllClaims()
+        public async Task<ActionResult<List<Claims>>> GetAllClaims()
         {
             var response = CA.Domain.Constants.Permission.Permissions.AllPermision.GetAllPermision();
             return Ok(response);
         }
 
-        /// <summary>
-        /// Add a Role Claim
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns>Status 200 OK </returns>
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [Authorize(Policy = Permissions.RoleClaimsPermissions.Create)]
         [HttpPost]
         public async Task<IActionResult> Post(RoleClaimRequest request)
         {
 
             await _mediator.Send(new InsertRoleClaimCommand() { Model = request});
-            return Ok();
+            return NoContent();
         }
 
-        /// <summary>
-        /// Delete a Role Claim
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Status 200 OK</returns>
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [Authorize(Policy = Permissions.RoleClaimsPermissions.Delete)]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             await _mediator.Send(new DeleteRoleClaimCommand() { Id = id });
-            return Ok();
+            return NoContent();
         }
     }
 }
