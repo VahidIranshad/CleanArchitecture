160ee7b6c8ea7e93f8e28f41b3f64f379fe6eba3
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-10-14 14:20:39 +0330

    edit migration

--- src/Infrastructure/CA.Infrastructure/Migrations/20231014091320_last.Designer.cs
+++ src/Infrastructure/CA.Infrastructure/Migrations/20231014091320_last.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CA.Infrastructure.Migrations
 {
     [DbContext(typeof(CustomDbContext))]
-    [Migration("20230925110836_First")]
-    partial class First
+    [Migration("20231014091320_last")]
+    partial class last
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -58,9 +58,10 @@ namespace CA.Infrastructure.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                    b.HasKey("Id")
+                        .HasName("PK_EntityLogDbSet");
 
-                    b.ToTable("EntityLogDbSet");
+                    b.ToTable("EntityLogDbSet", (string)null);
                 });
 
             modelBuilder.Entity("CA.Domain.Ent.Selection", b =>
@@ -98,7 +99,54 @@ namespace CA.Infrastructure.Migrations
                     b.HasKey("Id")
                         .HasName("PK_Ent_Selection");
 
+                    b.HasIndex("SelectionType", "Title")
+                        .IsUnique();
+
                     b.ToTable("Selection", "Ent");
+                });
+
+            modelBuilder.Entity("CA.Domain.Ent.TValue", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<DateTime>("CreateDate")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("CreatorID")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("LastEditDate")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("LastEditorID")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .HasColumnType("nvarchar(200)");
+
+                    b.HasKey("Id")
+                        .HasName("PK_Ent_TValue");
+
+                    b.ToTable("TValue", "Ent");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            CreateDate = new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            CreatorID = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                            LastEditDate = new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            LastEditorID = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                            Title = "Test"
+                        });
                 });
 #pragma warning restore 612, 618
         }
--- src/Infrastructure/CA.Infrastructure/Migrations/20231014091320_last.cs
+++ src/Infrastructure/CA.Infrastructure/Migrations/20231014091320_last.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace CA.Infrastructure.Migrations
 {
-    public partial class First : Migration
+    public partial class last : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -49,6 +49,37 @@ namespace CA.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_Ent_Selection", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "TValue",
+                schema: "Ent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastEditDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastEditorID = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ent_TValue", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Ent",
+                table: "TValue",
+                columns: new[] { "Id", "CreateDate", "CreatorID", "LastEditDate", "LastEditorID", "Title" },
+                values: new object[] { 1, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "8e445865-a24d-4543-a6c6-9443d048cdb9", new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "8e445865-a24d-4543-a6c6-9443d048cdb9", "Test" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Selection_SelectionType_Title",
+                schema: "Ent",
+                table: "Selection",
+                columns: new[] { "SelectionType", "Title" },
+                unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -58,6 +89,10 @@ namespace CA.Infrastructure.Migrations
 
             migrationBuilder.DropTable(
                 name: "Selection",
+                schema: "Ent");
+
+            migrationBuilder.DropTable(
+                name: "TValue",
                 schema: "Ent");
         }
     }
