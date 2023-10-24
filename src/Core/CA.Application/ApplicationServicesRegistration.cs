8150d1d6a55161d74ed5b3fa31cab6bd1cc89c7a
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-10-24 12:12:37 +0330

    remove MediatorServiceFactory

--- src/Core/CA.Application/ApplicationServicesRegistration.cs
+++ src/Core/CA.Application/ApplicationServicesRegistration.cs
@@ -41,36 +41,4 @@ namespace CA.Application
     {
         Type Convert(Type sourceType, ConverterDelegate next);
     }
-    public static class MediatorServiceFactory
-    {
-        public static ServiceFactory Wrap(ServiceFactory serviceFactory,
-            IEnumerable<IMediatorServiceTypeConverter> converters)
-        {
-            return serviceType =>
-            {
-                Type NoConversion() => serviceType;
-                var convertServiceType = converters
-                    .Reverse()
-                    .Aggregate((ConverterDelegate)NoConversion, (next, c) => () => c.Convert(serviceType, next));
-                return serviceFactory(convertServiceType());
-            };
-        }
-        public class TestConverter : IMediatorServiceTypeConverter
-        {
-            public Type Convert(Type sourceType, ConverterDelegate next)
-            {
-                var isRequestHandler = sourceType.IsGenericType &&
-                                       sourceType.GetGenericTypeDefinition() == typeof(IRequestHandler<,>);
-                if (!isRequestHandler) return next();
-
-                var requestType0 = sourceType.GenericTypeArguments[0];
-                var shouldConvertType = requestType0.IsGenericType &&
-                                            requestType0.GetGenericTypeDefinition() == typeof(GetListBaseQuery<,>);
-                if (!shouldConvertType) return next();
-                var returnType0 = requestType0.GenericTypeArguments[0];
-
-                return typeof(GetListBaseQueryHandler<,>).MakeGenericType(returnType0).MakeGenericType();
-            }
-        }
-    }
 }
