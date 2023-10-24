0a5d9720f7f0c4b2ffa146757feda1fdfe6f6455
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-10-24 12:31:05 +0330

    add test for role claim

--- test/FunctionalTests/Controllers/Identity/RoleClaimControllerTest.cs
+++ test/FunctionalTests/Controllers/Identity/RoleClaimControllerTest.cs
@@ -1,4 +1,7 @@
-﻿using FunctionalTests.Controllers.Common;
+﻿using CA.Application.DTOs.Identity.Responses;
+using CA.Domain.Base;
+using FunctionalTests.Controllers.Common;
+using Newtonsoft.Json;
 
 namespace FunctionalTests.Controllers.Identity
 {
@@ -7,6 +10,48 @@ namespace FunctionalTests.Controllers.Identity
         public RoleClaimControllerTest(CustomWebApplicationFactory<Program> factory) : base(factory)
         {
 
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsList_ReturnData()
+        {
+            var client = this.GetNewClientByAdminAuthorization();
+            var response = await client.GetAsync($"/api/identity/roleClaim");
+            response.EnsureSuccessStatusCode();
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<RoleClaimResponse>>(stringResponse);
+            var statusCode = response.StatusCode.ToString();
+
+            Assert.Equal("OK", statusCode);
+        }
+
+        [Fact]
+        public async Task GetAllByRoleId_ReturnsList_ReturnData()
+        {
+            var client = this.GetNewClientByAdminAuthorization();
+            var response = await client.GetAsync($"/api/identity/roleClaim?roleId={CA.Domain.Constants.Identity.RoleConstants.AdministratorRoleID}");
+            response.EnsureSuccessStatusCode();
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<RoleClaimResponse>>(stringResponse);
+            var statusCode = response.StatusCode.ToString();
+
+            Assert.Equal("OK", statusCode);
+        }
+
+        [Fact]
+        public async Task GetAllClaims_ReturnsList_ReturnData()
+        {
+            var client = this.GetNewClientByAdminAuthorization();
+            var response = await client.GetAsync($"/api/identity/roleClaim/GetAllClaims");
+            response.EnsureSuccessStatusCode();
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Claims>>(stringResponse);
+            var statusCode = response.StatusCode.ToString();
+
+            Assert.Equal("OK", statusCode);
         }
 
     }
